e48d42b022a0e6ba5b72955d71a837a2f3f59ca8
syJoe <csy5501@gmail.com>
2023-05-31 09:49:12 +0900

    0531 conditional statements

---
+++ HTML-CSS Practice/0531default.js
@@ -0,0 +1,60 @@
+//var cels = parseFloat(prompt("Enter the Celsius: "));
+//var fahrenheit = cels * (9 / 5) + 32;
+//
+//document.write("Fahrenheit : " + fahrenheit);
+
+//var num1 = 20;
+//var num2 = 10;
+//var num3 = "10";
+//var num4 = 20;
+//var num5 = 15;
+//
+//console.log(num1 > num2);
+//console.log(num1 >= num2)
+//console.log(num1 < num2);
+//console.log(num1 <= num2);
+//
+//console.log('같은지 여부 확인');
+//console.log(num1 == num4, num1, '==', num4);
+//console.log(num1 != num4, num1, "!=", num4);
+//
+//console.log('=== explain');
+//// '===' : data type compare
+//console.log(num1 === num3, num1, "===", num3);
+//
+//console.log(num2 === num3, num2, "===", num3);
+//console.log(num2 == num3, num2, "==", num3);
+////문자열("10") 숫자(10)가 달라도 숫자가 같으면 true가 출력된다. (유일한 다른점)
+//
+//console.log('num1 > num2 && num1 < num5 : ', num1 > num2 && num1 < num5);
+//console.log('num1 > num2 || num1 < num5 : ', num1 > num2 || num1 < num5);
+
+//var num1 = parseInt(prompt('첫번째 숫자 임력 : '));
+//var num2 = parseInt(prompt('두번째 숫자 임력 : '));
+//
+//if (num1 > num2) {
+//    console.log("큰 수는 num1 : ", + num1);
+//}
+//if (num1 < num2) {
+//    console.log('큰 수는 num2 : ' + num2);
+//}
+////num1 num2, 두 수가 같으면 아무것도 실행히 되지 않는다.
+//if (num1 == num2) {
+//    console.log("same number");
+//}
+//
+//if (num1 > num2) {
+//    console.log("큰 수는 num1 : ", + num1);
+//} else if (num1 < num2) {
+//    console.log("큰 수는 num2 : ", + num1);
+//} else {
+//    console.log("같은 수");
+//}
+
+//2교시 --------------------------------------------------------------
+
+//3교시 --------------------------------------------------------------
+
+//4교시 --------------------------------------------------------------
+
+//5교시 --------------------------------------------------------------

0db6bd70aa3b4b8b29e9bf53ac6efb1fa0eb3594
성열 <csy5501@gmail.com>
2023-06-12 13:18:00 +0900

    0612 custom exception

--- python/0612main.py
+++ python/0612main.py
@@ -90,3 +90,13 @@ except ValueError:
     print("value error")
 except TypeError:
     print("type error")
+
+#
+class CustomException(Exception):
+    def __init__(self, message):
+        self.message = message
+
+try:
+    raise CustomException("This is a custom exception.")
+except CustomException as e:
+    print(f"Error : {e.message}")

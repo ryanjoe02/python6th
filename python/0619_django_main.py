10026c88962b8963177f8903d0791ce3fb5c3186
성열 <csy5501@gmail.com>
2023-06-19 14:43:52 +0900

    0619 이미지 경로 추출, parser practice

--- python/0619_django_main.py
+++ python/0619_django_main.py
@@ -1,4 +1,6 @@
 import urllib.request
+from urllib.request import urlopen
+from html.parser import HTMLParser
 
 url = 'https://www.google.com'
 
@@ -7,4 +9,38 @@ response = urllib.request.urlopen(request)
 
 html = response.read()
 
-print(html)
+#print(html)
+
+class ImageParser(HTMLParser):
+    def __init__(self):
+        super().__init__()
+        self.result = []
+
+    def handle_starttag(self, tag, attrs):
+        if tag != 'img':
+            return
+        if not hasattr(self, 'result'):
+            self.result = []
+        
+        for name, value in attrs:
+            if name == 'src':
+                self.result.append(value)
+    
+def parse_image(data):
+    parser = ImageParser()
+    parser.feed(data)
+    dataSet = set(x for x in parser.result)
+    return dataSet
+
+def main():
+    url = 'https://google.co.kr'
+    with urlopen(url) as f:
+        charset = f.headers.get_params('charset')[1][1]
+        data = f.read().decode(charset)
+
+    data_set = parse_image(data)
+    print("\n>>>>> Fatch Images from ", url)
+    print("\n".join(sorted(data_set)))
+
+if __name__ == '__main__':
+    main()

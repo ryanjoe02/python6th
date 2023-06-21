227d3d546ab4fa543b5a5e65d100eda14691763d
성열 <csy5501@gmail.com>
2023-06-21 13:25:15 +0900

    0621 update views, urls

--- myproject/polls/views.py
+++ myproject/polls/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(requests):
+    pass
+
+def detail(requests):
+    pass
+
+def vote(requests):
+    pass
+
+def results(requests):
+    pass

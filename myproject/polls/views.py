6c27112899b4ee9a565a3fbefe5b111e1c590a02
성열 <csy5501@gmail.com>
2023-06-22 13:56:26 +0900

    0622 practiced in views.py & index.html

--- myproject/polls/views.py
+++ myproject/polls/views.py
@@ -4,11 +4,10 @@ from django.shortcuts import render, get_object_or_404
 
 from polls.models import Question, Choice
 
-
 # Create your views here.
 def index(request):
     latest_question_list = Question.objects.all().order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
+    context = {'latest_question_list': latest_question_list, 'value': '<strong>hello, world</strong>'}
     return render(request, 'polls/index.html', context)
 
 
